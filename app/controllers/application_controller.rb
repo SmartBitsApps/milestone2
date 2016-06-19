5fcf4cc52f7bf45ee90409fd85b5dff892ad6a64
SmartBitsApps <gapparena@gmail.com>
2016-06-19 17:14:47 +0000

    adds update and destroy users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def user_not_authorized
-    flash[:alert] = "Access denied."
-    redirect_to (request.referrer || root_path)
-  end
+    def user_not_authorized
+      flash[:alert] = "Access denied."
+      redirect_to (request.referrer || root_path)
+    end
+    
 end
