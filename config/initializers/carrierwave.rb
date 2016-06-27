fabdd96d734e4d1a0e6e5656dcdac7437b62a4a3
SmartBitsApps <gapparena@gmail.com>
2016-06-27 16:42:58 +0000

    adds cache and secure in carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ CarrierWave.configure do |config|
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = ENV['S3_BUCKET']                          # required
-  #config.cache_dir      = "#{Rails.root}/tmp/uploads"               # to let working on Heroku
- # config.fog_public     = false                                        # optional, defaults to true
-  #config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  config.cache_dir      = "#{Rails.root}/tmp/uploads"               # to let working on Heroku
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
 
 module CarrierWave
